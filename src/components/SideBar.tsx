abf67c39d95a8a4486e1ade44bec959a8fdd3741
Rafael Domingues <rafaelcodomingues@gmail.com>
2021-06-08 16:32:23 -0300

    Finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
